63ce9c4098e7656a076b28c1943a42f31c949139
Marc Kloter <marc.kloter@bluewin.ch>
2017-01-10 09:05:56 +0100

    added dist task as default

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var connect = require('gulp-connect-php');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var del = require('del');
+var uglify = require('gulp-uglify');
+var svgmin = require('gulp-svgmin');
+var jsonminify = require('gulp-jsonminify');
 
 gulp.task('sass', function () {
   return gulp.src('app/style/*.scss')
@@ -21,6 +25,50 @@ gulp.task('serve', ['sass:watch'], function() {
   gulp.watch('app/style/**/*.css').on('change', browserSync.reload);
   gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 });
+
+gulp.task('clean', function() {
+  return del('dist');
+});
+
+gulp.task('copy', ['clean'], function() {
+  var app = gulp.src([
+    'app/**',
+    '!app/style/**',
+    '!app/style',
+    '!app/js/**',
+    '!app/js',
+    '!app/*.json',
+    '!app/img/*.svg'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', ['clean'], function(){
+  return gulp.src('app/style/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(gulp.dest('dist/style'));
+});
+
+gulp.task('minify-json', ['clean'], function(){
+  return gulp.src('app/*.json')
+      .pipe(jsonminify())
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js', ['clean'], function(){
+  return gulp.src('app/js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-svg', ['clean'], function(){
+  return gulp.src('app/img/*.svg')
+      .pipe(svgmin())
+      .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', ['copy', 'minify-css', 'minify-js', 'minify-svg', 'minify-json']);
 
 // avoid crash on error
 function swallowError (error) {
